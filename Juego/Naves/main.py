d6c4e2bc0e3696fd7cd6cf0dbece726fe742af8f
ONieto7 <oscarniro@gmail.com>
2025-03-13 18:10:55 +0100

    Movimientos de jugador y enemigos

--- Juego/Naves/main.py
+++ Juego/Naves/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Inicialización de Pygame
 pygame.init()
@@ -10,24 +11,52 @@ pantalla = pygame.display.set_mode((800, 600))
 pygame.display.set_caption(" Invasión Espacial ")
 icono = pygame.image.load("ovni.png")
 pygame.display.set_icon(icono)
+fondo = pygame.image.load("fondo.jpg")
 
-# Jugador
+# Variables del Jugador
 img_jugador = pygame.image.load("astronave.png")
 jugador_x = 368
-jugador_y = 536
+jugador_y = 520
 jugador_x_cambio = 0
+
+# Variables del Enemigo
+img_enemigo = pygame.image.load("enemigo.png")
+enemigo_x = random.randint(0, 736)
+enemigo_y = random.randint(50, 200)
+enemigo_x_cambio = 0.1
+enemigo_y_cambio = 30
+
+# Variables de la Bala
+img_bala = pygame.image.load("explosion.png")
+bala_x = 0
+bala_y = 480
+bala_x_cambio = 0
+bala_y_cambio = 0.4
+bala_visible = False
 
 # Función para mostrar al jugador
 def jugador(x, y):
     pantalla.blit(img_jugador, (x, y))
 
 
+# Función para mostrar al enemigo
+def enemigo(x, y):
+    pantalla.blit(img_enemigo, (x, y))
+
+
+# Función disparar bala
+def disparar_bala(x, y):
+    global bala_visible
+    bala_visible = True
+    pantalla.blit(img_bala, (x + 16, y + 10))
+
+
 # Bucle principal
 se_ejecuta = True
 while se_ejecuta:
 
-    # Color de fondo RGB
-    pantalla.fill((205, 144, 228))
+    # Imagen de fondo
+    pantalla.blit(fondo, (0, 0))
     
     # Eventos
     for evento in pygame.event.get():
@@ -42,6 +71,10 @@ while se_ejecuta:
                 jugador_x_cambio -= 0.1
             if evento.key == pygame.K_RIGHT:
                 jugador_x_cambio += 0.1
+            if evento.key == pygame.K_SPACE:
+                if not bala_visible:
+                    bala_x = jugador_x
+                    disparar_bala(bala_x, bala_y)
 
         # Detener el movimiento del jugador
         if evento.type == pygame.KEYUP:
@@ -50,7 +83,35 @@ while se_ejecuta:
 
     # Actualizar la posición del jugador
     jugador_x += jugador_x_cambio
+
+    # Límites del jugador
+    if jugador_x <= 0:
+        jugador_x = 0
+    elif jugador_x >= 736:
+        jugador_x = 736
+
+    # Actualizar la posición del enemigo
+    enemigo_x += enemigo_x_cambio
+
+    # Límites del enemigo
+    if enemigo_x <= 0:
+        enemigo_x_cambio = 0.1
+        enemigo_y += enemigo_y_cambio
+    elif enemigo_x >= 736:
+        enemigo_x_cambio = -0.1
+        enemigo_y += enemigo_y_cambio
+
+    # Movimiento de la bala
+    if bala_y <= -64:
+        bala_y = 480
+        bala_visible = False
+
+    if bala_visible:
+        disparar_bala(bala_x, bala_y)
+        bala_y -= bala_y_cambio
+
     jugador(jugador_x, jugador_y)
+    enemigo(enemigo_x, enemigo_y)
 
     # Actualizar la pantalla
     pygame.display.update()
