420a2462a27c0b73041d91743a6b32f34ba386de
ONieto7 <oscarniro@gmail.com>
2025-04-07 17:16:14 +0200

    Coregir funcionalidad para calcular costos y generar recibos en el sistema de facturación

--- Restaurante/mi_restaurante.py
+++ Restaurante/mi_restaurante.py
@@ -4,6 +4,185 @@ import datetime
 from tkinter import filedialog, messagebox
 
 
+operador = ''
+precios_comida = [1.32, 1.65, 2.31, 3.22, 1.22, 1.99, 2.05, 2.65]
+precios_bebida = [0.25, 0.99, 1.21, 1.54, 1.08, 1.10, 2.00, 1.58]
+precios_postres = [1.54, 1.68, 1.32, 1.97, 2.55, 2.14, 1.94, 1.74]
+
+
+def click_boton(numero):
+    global operador
+    operador = operador + numero
+    visor_calculadora.delete(0, END)
+    visor_calculadora.insert(END, operador)
+
+
+def borrar():
+    global operador
+    operador = ''
+    visor_calculadora.delete(0, END)
+
+
+def obtener_resultado():
+    global operador
+    resultado = str(eval(operador))
+    visor_calculadora.delete(0, END)
+    visor_calculadora.insert(0, resultado)
+    operador = ''
+
+
+def revisar_check():
+    x = 0
+    for c in cuadros_comida:
+        if variables_comida[x].get() == 1:
+            cuadros_comida[x].config(state=NORMAL)
+            if cuadros_comida[x].get() == '0':
+                cuadros_comida[x].delete(0, END)
+            cuadros_comida[x].focus()
+        else:
+            cuadros_comida[x].config(state=DISABLED)
+            texto_comida[x].set('0')
+        x += 1
+
+    x = 0
+    for c in cuadros_bebida:
+        if variables_bebida[x].get() == 1:
+            cuadros_bebida[x].config(state=NORMAL)
+            if cuadros_bebida[x].get() == '0':
+                cuadros_bebida[x].delete(0, END)
+            cuadros_bebida[x].focus()
+        else:
+            cuadros_bebida[x].config(state=DISABLED)
+            texto_bebida[x].set('0')
+        x += 1
+
+    x = 0
+    for c in cuadros_postres:
+        if variables_postres[x].get() == 1:
+            cuadros_postres[x].config(state=NORMAL)
+            if cuadros_postres[x].get() == '0':
+                cuadros_postres[x].delete(0, END)
+            cuadros_postres[x].focus()
+        else:
+            cuadros_postres[x].config(state=DISABLED)
+            texto_postres[x].set('0')
+        x += 1
+
+
+def total():
+    sub_total_comida = 0
+    p = 0
+    for cantidad in texto_comida:
+        sub_total_comida = sub_total_comida + (float(cantidad.get()) * precios_comida[p])
+        p += 1
+
+    sub_total_bebida = 0
+    p = 0
+    for cantidad in texto_bebida:
+        sub_total_bebida = sub_total_bebida + (float(cantidad.get()) * precios_bebida[p])
+        p += 1
+
+    sub_total_postres = 0
+    p = 0
+    for cantidad in texto_postres:
+        sub_total_postres = sub_total_postres + (float(cantidad.get()) * precios_postres[p])
+        p += 1
+
+    sub_total = sub_total_comida + sub_total_bebida + sub_total_postres
+    impuestos = sub_total * 0.07
+    total = sub_total + impuestos
+
+    var_costo_comida.set(f'$ {round(sub_total_comida, 2)}')
+    var_costo_bebida.set(f'$ {round(sub_total_bebida, 2)}')
+    var_costo_postres.set(f'$ {round(sub_total_postres, 2)}')
+    var_subtotal.set(f'$ {round(sub_total, 2)}')
+    var_impuestos.set(f'$ {round(impuestos, 2)}')
+    var_total.set(f'$ {round(total, 2)}')
+
+
+def recibo():
+    texto_recibo.delete(1.0, END)
+    num_recibo = f'N# - {random.randint(1000, 9999)}'
+    fecha = datetime.datetime.now()
+    fecha_recibo = f'{fecha.day}/{fecha.month}/{fecha.year} - {fecha.hour}:{fecha.minute}'
+    texto_recibo.insert(END, f'Datos:\t{num_recibo}\t\t{fecha_recibo}\n')
+    texto_recibo.insert(END, f'*' * 47 + '\n')
+    texto_recibo.insert(END, 'Items\t\tCant.\tCosto Items\n')
+    texto_recibo.insert(END, f'-' * 54 + '\n')
+
+    x = 0
+    for comida in texto_comida:
+        if comida.get() != '0':
+            texto_recibo.insert(END, f'{lista_comidas[x]}\t\t{comida.get()}\t'
+                                     f'$ {int(comida.get()) * precios_comida[x]}\n')
+        x += 1
+
+    x = 0
+    for bebida in texto_bebida:
+        if bebida.get() != '0':
+            texto_recibo.insert(END, f'{lista_bebidas[x]}\t\t{bebida.get()}\t'
+                                     f'$ {int(bebida.get()) * precios_bebida[x]}\n')
+        x += 1
+
+    x = 0
+    for postres in texto_postres:
+        if postres.get() != '0':
+            texto_recibo.insert(END, f'{lista_postres[x]}\t\t{postres.get()}\t'
+                                     f'$ {int(postres.get()) * precios_postres[x]}\n')
+        x += 1
+
+    texto_recibo.insert(END, f'-' * 54 + '\n')
+    texto_recibo.insert(END, f' Costo de la Comida: \t\t\t{var_costo_comida.get()}\n')
+    texto_recibo.insert(END, f' Costo de la Bebida: \t\t\t{var_costo_bebida.get()}\n')
+    texto_recibo.insert(END, f' Costo de la Postres: \t\t\t{var_costo_postres.get()}\n')
+    texto_recibo.insert(END, f'-' * 54 + '\n')
+    texto_recibo.insert(END, f' Sub-total: \t\t\t{var_subtotal.get()}\n')
+    texto_recibo.insert(END, f' Impuestos: \t\t\t{var_impuestos.get()}\n')
+    texto_recibo.insert(END, f' Total: \t\t\t{var_total.get()}\n')
+    texto_recibo.insert(END, f'*' * 47 + '\n')
+    texto_recibo.insert(END, 'Lo esperamos pronto')
+
+
+def guardar():
+    info_recibo = texto_recibo.get(1.0, END)
+    archivo = filedialog.asksaveasfile(mode='w', defaultextension='.txt')
+    archivo.write(info_recibo)
+    archivo.close()
+    messagebox.showinfo('Informacion', 'Su recibo ha sido guardado')
+
+
+def resetear():
+    texto_recibo.delete(0.1, END)
+
+    for texto in texto_comida:
+        texto.set('0')
+    for texto in texto_bebida:
+        texto.set('0')
+    for texto in texto_postres:
+        texto.set('0')
+
+    for cuadro in cuadros_comida:
+        cuadro.config(state=DISABLED)
+    for cuadro in cuadros_bebida:
+        cuadro.config(state=DISABLED)
+    for cuadro in cuadros_postres:
+        cuadro.config(state=DISABLED)
+
+    for v in variables_comida:
+        v.set(0)
+    for v in variables_bebida:
+        v.set(0)
+    for v in variables_postres:
+        v.set(0)
+
+    var_costo_comida.set('')
+    var_costo_bebida.set('')
+    var_costo_postres.set('')
+    var_subtotal.set('')
+    var_impuestos.set('')
+    var_total.set('')
+
+
 # iniciar tkinter
 aplicacion = Tk()
 
