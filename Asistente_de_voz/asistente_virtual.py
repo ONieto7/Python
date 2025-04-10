a84a6920a7d7f1a84a10cf2d95d25a50f6d15801
ONieto7 <oscarniro@gmail.com>
2025-04-10 12:11:15 +0200

    Refactorizar la función de reconocimiento de voz y mejorar la gestión de errores

--- Asistente_de_voz/asistente_virtual.py
+++ Asistente_de_voz/asistente_virtual.py
@@ -7,46 +7,76 @@ import webbrowser
 import datetime
 import wikipedia
 
+# opciones de voz / idioma
 id1 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0'
 id2 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
 id3 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-ES_HELENA_11.0'
 id4 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0'
 
 
+# escuchar nuestro microfono y devolver el audio comotexto
 def trasformar_audio_en_texto():
 
+    # almacenar recognizer en variable
     r = sr.Recognizer()
 
+    # configurar el microfono
     with sr.Microphone() as origen:
 
+        # tiempo de espera
         r.pause_threshold = 0.8
 
+        # informar que comenzo la grabacion
         print("ya puedes hablar")
 
+        # guardar lo que escuche como audio
         audio = r.listen(origen)
 
         try:
-   
+            # buscar en google
             pedido = r.recognize_google(audio, language="es-ar")
 
+            # prueba de que pudo ingresar
             print("Dijiste: " + pedido)
 
+            # devolver pedido
             return pedido
 
+        # en caso de que no comprenda el audio
         except sr.UnknownValueError:
 
+            # prueba de que no comprendio el audio
             print("ups, no entendi")
 
+            # devolver error
             return "sigo esperando"
 
+        # en caso de no resolver el pedido
         except sr.RequestError:
 
+            # prueba de que no comprendio el audio
             print("ups, no hay servicio")
 
+            # devolver error
             return "sigo esperando"
 
+        # error inesperado
         except:
 
+            # prueba de que no comprendio el audio
             print("ups, algo ha salido mal")
 
+            # devolver error
             return "sigo esperando"
+        
+        
+# funcion para que el asistente pueda ser escuchado
+def hablar(mensaje):
+
+    # encender el motor de pyttsx3
+    engine = pyttsx3.init()
+    engine.setProperty('voice', id3)
+
+    # pronunciar mensaje
+    engine.say(mensaje)
+    engine.runAndWait()
